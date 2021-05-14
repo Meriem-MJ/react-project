8af55e9b308adab37a3516a1a33b2284a2b3dd2f
Meriem <jebara.meriem133@gmail.com>
2021-05-14 14:46:17 +0200

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-     <h1>
-       Hello
-     </h1>
+    <Navbar/>
     </div>
   );
 }
