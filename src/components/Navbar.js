657f05599030dc2f890453d0f7da33cf5fe8fecd
Meriem <jebara.meriem133@gmail.com>
2021-05-14 23:16:15 +0200

    Header Complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item"><a className="nav-link active" aria-current="page" href="#">Home</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">About Me</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">About Us</a></li>
                             <li className="nav-item"><a className="nav-link" href="#">Projects</a></li>
                             <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
                         </ul>
